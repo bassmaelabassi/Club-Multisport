2fcb868e8a6d0f2fdfeb60d00aaedd9ff1e2b2ba
bassmaelabassi <elabassibasma31@gmail.com>
2025-06-14 22:09:33 +0200

    ajout la structure des dossiers

---
+++ frontend/src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <div>
+       <h1 className="text-4xl font-bold text-red-500">Tailwind fonctionne ✅</h1>
+      <p className="mt-4 text-gray-600">Bienvenue dans ton projet React + Tailwind</p>
+    </div>
+  )
+}
