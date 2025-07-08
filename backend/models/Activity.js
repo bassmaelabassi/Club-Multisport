123d8697d4eb34d034a82ddd9368bca3eeba1dd8
bassmaelabassi <elabassibasma31@gmail.com>
2025-07-08 15:55:41 +0200

    Créer le modèle Review

--- backend/models/Activity.js
+++ backend/models/Activity.js
@@ -1,21 +1,23 @@
-const { required } = require('joi');
 const mongoose = require('mongoose');
 
-const isActiveSchema = new mongoose.Schema({
-  name: { type: String, required: true};
-  category: { type: String, required: true, enum: ['danse', 'musique', 'natation', 'equitation', 'fitness','autre']
+const ActivitySchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  category: { 
+    type: String, 
+    required: true,
+    enum: ['danse', 'musique', 'natation', 'équitation', 'fitness', 'autre'] 
   },
-  description: { type: String, required: true},
-  coach: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true},
+  description: { type: String, required: true },
+  coach: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
   schedule: [
     {
-      day: { type: String, enum: ['lundi', 'mardi', 'mercredi', 'jeudi', 'vendredi', 'samedi', 'dimanche']}
-      startTime: { type: String},
-      endTime: { type: String},
-      maxParticipants: { type: Number},
+      day: { type: String, enum: ['lundi', 'mardi', 'mercredi', 'jeudi', 'vendredi', 'samedi', 'dimanche'] },
+      startTime: { type: String },
+      endTime: { type: String },
+      maxParticipants: { type: Number },
     }
   ],
-  price: { type: Number, required: true},
+  price: { type: Number, required: true },
   duration: { type: Number, required: true },
   location: { type: String, required: true },
   image: { type: String },
@@ -23,4 +25,4 @@ const isActiveSchema = new mongoose.Schema({
   createdAt: { type: Date, default: Date.now },
 });
 
-module.export = mongoose.model('Activity', isActiveSchema);
+module.exports = mongoose.model('Activity', ActivitySchema);
--- backend/models/Review.js
+++ backend/models/Review.js
@@ -1,3 +1,12 @@
+const mongoose = require('mongoose');
 
+const ReviewSchema = new mongoose.Schema({
+  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', requiredd: true},
+   activity: { type: mongoose.Schema.Types.ObjectId, ref: 'Activity', required: true },
+  coach: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  rating: { type: Number, min: 1, max: 5, required: true },
+  comment: { type: String },
+  createdAt: { type: Date, default: Date.now },
+});
 
-
+module.exports = mongoose.model('Review',ReviewSchema);
